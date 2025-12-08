4a960bb43d36a3d2d9332f012630226644a009c2
Joby-John <joby432john@gmail.com>
2025-12-08 21:37:19 +0530

    feat: initialize cart with localStorage data and ensure data persistence on add/remove actions using local storage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,12 +1,5 @@
 
-export let cart = [{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2
-},{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
-}
-];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export function addToCart(productId){
         let matchingItem;
@@ -27,6 +20,8 @@ export function addToCart(productId){
                 quantity:1,
             });
         }
+
+        saveToStorage();
     }
 
 export function addToCartAnimation(button){
@@ -58,5 +53,10 @@ export function removeFromCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
 
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
