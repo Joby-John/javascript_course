dbecf8390c99171fae737ab687469c7b4364fe38
Joby-John <joby432john@gmail.com>
2025-12-11 22:53:24 +0530

    feat: restructure Jasmine tests by removing old files and adding new test suite for formatCurrency function

---
+++ javascript-amazon-project/tests-jasmine/moneyTest.js
@@ -0,0 +1,23 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+
+
+describe('Test suite: formatCurrency', ()=>{
+    it('Converts cents to dollars', ()=>{
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('Works with 0', ()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    describe('Rounds', ()=>{
+        it('Rounds up to nearest cent', ()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+        });
+
+        it('Rounds down to nearest cent', ()=>{
+            expect(formatCurrency(2000.4)).toEqual('20.00');
+        });
+    });
+});
