531a140f0cfa6c00a15d63e9c69fda6b37426a97
Joby-John <joby432john@gmail.com>
2025-12-14 13:14:13 +0530

    feat: created checkout function susing class

---
+++ javascript-amazon-project/data/cart-class.js
@@ -0,0 +1,130 @@
+import { products } from "./products.js";
+
+
+class Cart{
+    cartItems = [];
+    localStorageKey;
+
+    //constructor
+    constructor(localStorageKey){
+        this.localStorageKey = localStorageKey;
+        this.initCart();
+    }
+
+    loadFromStorage(){
+        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+    };
+    
+    saveToStorage(){
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    };
+
+    initCart(){
+        this.loadFromStorage();
+    };
+    
+    addToCart(productId){
+        const productExists = (products.some(product => product.id === productId ));
+
+        if(productExists){
+            let matchingItem;
+
+            this.cartItems.forEach((cartItem) =>{
+                if(cartItem.productId === productId){
+                    matchingItem = cartItem;
+                }
+
+            });
+
+            if(matchingItem){
+                matchingItem.quantity += 1;
+            }else{
+                this.cartItems.push(
+                {
+                    productId: productId,
+                    quantity:1,
+                    deliveryOptionId :'1'
+                });
+            }
+
+            this.saveToStorage();
+        }
+
+    };
+
+    addToCartAnimation(button){
+        // For the "Added to Cart" animation:
+        const addedTocartDiv = button.closest('.js-product-container').querySelector('.js-added-to-cart');
+
+        // In JavaScript, every object can have custom properties.
+        // Here, we attach a `timeOutId` property to each product's addedTocartDiv
+        // This ensures each product card has its own independent timeout.
+        if(addedTocartDiv.timeOutId){
+            clearTimeout(addedTocartDiv.timeOutId);// cancel any previous timeout if user clicks again quickly
+        }
+
+        addedTocartDiv.classList.add('added-to-cart-vis');
+
+        addedTocartDiv.timeOutId = setTimeout(()=>{
+            addedTocartDiv.classList.remove('added-to-cart-vis');
+
+            addedTocartDiv.timeOutId = null;// clean up the custom property after the animation ends
+        }, 2000);
+    };
+
+    removeFromCart(productId){
+    let newCart = [];
+
+    this.cartItems.forEach((product)=>{
+        if(product.productId !== productId){
+            newCart.push(product);
+        }
+    });
+
+    const productContainer = document.querySelector(`[data-cart-item-id="${productId}"]`);
+    
+    if(productContainer){
+        productContainer.remove();
+    }
+
+    this.cartItems = newCart;
+    this.saveToStorage();
+
+    };
+
+    countCart(){
+    let cartQuantity = 0;
+    this.cartItems.forEach((cartItem) =>{
+            cartQuantity += cartItem.quantity;
+        });
+    return cartQuantity;
+    };
+
+
+    updateDeliveryOption(productId, deliveryOptionId){
+
+        const matchingItem = this.cartItems.find((item) => 
+        {
+            return item.productId === productId
+        });
+
+        if(matchingItem){
+            matchingItem.deliveryOptionId = deliveryOptionId;
+            this.saveToStorage();
+        }
+
+    };
+};
+
+
+const cart = new Cart('ind-cart');
+const businessCart = new Cart('bus-cart');
+
+cart.addToCart('901eb2ca-386d-432e-82f0-6fb1ee7bf969');
+businessCart.addToCart('04701903-bc79-49c6-bc11-1af7e3651358');
+businessCart.addToCart('901eb2ca-386d-432e-82f0-6fb1ee7bf969');
+
+console.log(cart);
+console.log(businessCart);
+
+console.log(businessCart instanceof Cart);
