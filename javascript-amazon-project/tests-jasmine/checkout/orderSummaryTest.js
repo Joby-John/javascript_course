1ac91e0fadffeb0bd10bf823dcbaef5678d59d3d
Joby-John <joby432john@gmail.com>
2025-12-14 11:10:37 +0530

    refactor: streamline product ID definitions and enhance cart item verification in order summary tests

--- javascript-amazon-project/tests-jasmine/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests-jasmine/checkout/orderSummaryTest.js
@@ -1,11 +1,13 @@
 import { setupOrderSummaryDOM } from "./setUpTestDOMCheckout.js";
 import { renderOrderSummmary } from "../../scripts/checkout/orderSummary.js";
-import { initCart } from "../../data/cart.js";
+import { initCart, cart } from "../../data/cart.js";
 
 setupOrderSummaryDOM();
 
 describe('Test Suite : renderOrderSummary', () => {
-    
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productId2 = '3fdfe8d6-9a15-4979-b459-585b0d0545b9';
+
     beforeEach(() => {
         setupOrderSummaryDOM(); 
         
@@ -31,11 +33,6 @@ describe('Test Suite : renderOrderSummary', () => {
     it('removes a product from the cart', () => {
         renderOrderSummmary();
 
-        // 1. Define the ID of the product you want to delete 
-        // (matches the mock data in your beforeEach)
-        const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-        const productId2 = '3fdfe8d6-9a15-4979-b459-585b0d0545b9';
-
         // 2. Select the specific delete link using the Attribute Selector
         // Syntax: .class[data-attribute="value"]
         const deleteLink = document.querySelector(`.js-delete-quantity-link[data-product-id="${productId1}"]`);
@@ -43,24 +40,26 @@ describe('Test Suite : renderOrderSummary', () => {
         // 3. Click it programmatically
         deleteLink.click();
 
-        // 4. Verification: Check the DOM 
+       
         
-        // Expectation A: Total items should now be 1 (since we started with 2)
+       
         expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1);
 
-        // Expectation B: The container for the deleted product (ID1) should be null (gone)
+        
         const container1 = document.querySelector(`.js-cart-item-container[data-cart-item-id="${productId1}"]`);
         expect(container1).toEqual(null);
 
-        // Expectation C: The container for the OTHER product (ID2) should still exist
+        
         const container2 = document.querySelector(`.js-cart-item-container[data-cart-item-id="${productId2}"]`);
         expect(container2).not.toEqual(null);
+
+        expect(cart.length).toEqual(1);
 
     });
 
     it('update product quantity', ()=>{
+
         renderOrderSummmary();
-        const productId2 = '3fdfe8d6-9a15-4979-b459-585b0d0545b9';
 
         const inputField = document.querySelector(`.js-new-quantity-input[data-product-id="${productId2}"]`);
 
